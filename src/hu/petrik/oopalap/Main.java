ffaa1354c2ba0ad3d7917639c6cc523f66d292fd
Rimoczi Daniel <rimoczidani007@gmail.com>
2022-09-16 21:44:57 +0200

    a

--- src/hu/petrik/oopalap/Main.java
+++ src/hu/petrik/oopalap/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Pont p1 = new Pont();
         Pont p2 = new Pont(3,6);
         Pont p3 = new Pont(100);
+
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
