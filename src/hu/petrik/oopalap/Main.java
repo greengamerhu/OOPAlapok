13e02550f96c4c02f0f444a81d23ca48ffad5706
Rimoczi Daniel <rimoczidani007@gmail.com>
2022-09-16 17:12:24 +0200

    1.feladat kesz

---
+++ src/hu/petrik/oopalap/Main.java
@@ -0,0 +1,35 @@
+package hu.petrik.oopalap;
+
+import hu.petrik.oopalap.pont.Pont;
+
+public class Main {
+    public static void main(String[] args) {
+        Pont p1 = new Pont();
+        Pont p2 = new Pont(3,6);
+        Pont p3 = new Pont(100);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.printf("A(z) %s pont tavolsaga az origotol %.3f \n",p3, p3.getTavolasgOrigo() );
+        System.out.printf("A(z) %s pont tavolsaga az %s pontol %.3f \n",p2, p3, p2.tavolsag(p3) );
+
+        Pont[] pontok = new Pont[100];
+        for (int i = 0; i < pontok.length; i++) {
+            pontok[i] = new Pont(15);
+        }
+        for(Pont p: pontok){
+            System.out.println(p);
+
+        }
+        int legtavolabbiIndex = 0;
+
+        for (int i = 0; i < pontok.length; i++) {
+            if (pontok[legtavolabbiIndex].getTavolasgOrigo() < pontok[i].getTavolasgOrigo()) {
+                legtavolabbiIndex = i;
+            }
+
+        }
+        System.out.printf("Az origotol legtavolabb az %d áll " +
+                "A pomt az %s tavolsaga az origitol: %.3f\n", legtavolabbiIndex+1, pontok[legtavolabbiIndex],pontok[legtavolabbiIndex].getTavolasgOrigo() );
+    }
+}
---
+++ src/hu/petrik/oopalap/pont/Pont.java
@@ -0,0 +1,60 @@
+package hu.petrik.oopalap.pont;
+
+public class Pont {
+    private int x;
+    private int y;
+
+
+
+    public Pont() {
+        this.x =0;
+        this.y =0;
+    }
+
+
+    public Pont(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Pont(int n) {
+        this.x = kordinátaGeneralasa(n);
+        this.y = kordinátaGeneralasa(n);
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getTavolasgOrigo() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+    public  double tavolsag(Pont masikPont) {
+        return Math.sqrt(Math.pow(this.x - masikPont.x, 2) + Math.pow(this.y - masikPont.y, 2));
+    }
+
+
+
+    private static int kordinátaGeneralasa(int n) {
+        return (int) (Math.random() * (2 * n + 1)) - n;
+    }
+
+
+
+
+
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", this.x, this.y);
+    }
+}
